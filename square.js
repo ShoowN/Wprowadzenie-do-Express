a39d878d4af1cd8b45c5f15e55d05aa7dfd56ab2
ShoowN <72608637+ShoowN@users.noreply.github.com>
2021-12-14 14:07:25 +0100

    commit 4

--- square.js
+++ square.js
@@ -1,2 +1,10 @@
-exports.area = function(width) { return width * width; };
-exports.perimeter = function(width) { return 4 * width; };
+module.exports = {
+    area: function(width) {
+      return width * width;
+    },
+  
+    perimeter: function(width) {
+      return 4 * width;
+    }
+  };
+  
